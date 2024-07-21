847e05ac53d530f2dc3aa294eabbee2f1820faff
jemaie <jerome.maier@uzh.ch>
2024-07-21 21:33:07 +0200

    cors & test-endpoint spotify auth

--- server/app/Controller.py
+++ server/app/Controller.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
+from models.authCode import AuthCode
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
+
 
 @app.get("/")
 def read_root():
     return {"Hello": "World!"}
+
+
+@app.post("/auth-codes")
+def store_auth_code(auth_code: AuthCode):
+    return {"Test successful": "{}".format(auth_code)}
