a941ff3fd8ac4ccd071264ff45317199a827f99a
jemaie <jerome.maier@uzh.ch>
2024-07-21 21:37:16 +0200

    router adjustments for test views

--- client/src/App.vue
+++ client/src/App.vue
@@ -13,6 +13,8 @@ import HelloWorld from './components/HelloWorld.vue'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/auth">Spotify Auth (PoC)</RouterLink>
+        <RouterLink to="/callback">CallBack</RouterLink>
       </nav>
     </div>
   </header>
--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import SpotifyAuthView from '../views/SpotifyAuthView.vue'
+import CallbackView from "@/views/CallbackView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,6 +18,16 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/auth',
+      name: 'auth',
+      component: SpotifyAuthView
+    },
+    {
+      path: '/callback',
+      name: 'callback',
+      component: CallbackView
     }
   ]
 })
