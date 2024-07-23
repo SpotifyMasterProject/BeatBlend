0b6dce167e546bae4156a66bf428674daaf77205
jemaie <jerome.maier@uzh.ch>
2024-07-23 21:58:14 +0200

    import adjustments

--- client/src/main.ts
+++ client/src/main.ts
@@ -6,7 +6,6 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import PrimeVue from 'primevue/config'
-import Ripple from 'primevue/ripple'
 
 const app = createApp(App)
 
