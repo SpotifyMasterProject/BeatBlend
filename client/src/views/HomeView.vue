1dae336044db19f94134872476552e5bd63f8e96
saeie07 <115580828+saeie07@users.noreply.github.com>
2024-08-06 14:53:16 +0200

    first draft of flower petal

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,8 +1,15 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 import LogoIntroScreen from "@/components/LogoIntroScreen.vue";
-import { RouterLink } from 'vue-router';
 import Navigation from "@/components/Navigation.vue";
+import Flower from "@/components/Flower.vue";
+
+const flowerData = [
+  { value: 0.4, color: '#144550' },
+  { value: 0.6, color: '#31431E' },
+  { value: 0.5, color: '#EEE8C4' },
+  { value: 0.7, color: '#E4832E' },
+  { value: 0.3, color: '#BB7DEC' },
+]
 </script>
 
 <template>
@@ -20,7 +27,7 @@ import Navigation from "@/components/Navigation.vue";
 
     </header>
     <div class="middle">
-
+      <Flower :features="flowerData" :size="300" :circleRadius="100" />
     </div>
     <div class="previously-played">
 
