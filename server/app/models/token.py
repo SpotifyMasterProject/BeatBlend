56f1e817b08d2d3f75aa3506e96e488df2275ac5
maettuu <mangori41@bluewin.ch>
2024-08-19 16:45:51 +0200

    adjust token model for optional entries

--- server/app/models/token.py
+++ server/app/models/token.py
@@ -1,9 +1,10 @@
 from pydantic import BaseModel
+from typing import Optional
 
 
 class Token(BaseModel):
     access_token: str
     token_type: str
-    expires_in: int
-    refresh_token: str
-    scope: str
+    expires_in: Optional[int] = 0
+    refresh_token: Optional[str] = None
+    scope: Optional[str] = None
