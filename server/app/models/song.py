a51f9fb3eea8ec772461fa1a384c02d0b6471fbb
maettuu <mangori41@bluewin.ch>
2024-08-21 13:40:41 +0200

    update song model with postgres entries

--- server/app/models/song.py
+++ server/app/models/song.py
@@ -5,13 +5,16 @@ from typing import Optional, List
 
 class Song(BaseModel):
     id: Optional[str] = None
-    name: Optional[str] = None
-    artists: List[str] = []
+    track_name: Optional[str] = None
     album: Optional[str] = None
-    release_date: Optional[datetime] = None
-    popularity: Optional[float] = None
+    album_id: Optional[str] = None
+    artists: List[str] = []
+    artist_ids: List[str] = []
     danceability: Optional[float] = None
     energy: Optional[float] = None
     speechiness: Optional[float] = None
     valence: Optional[float] = None
     tempo: Optional[float] = None
+    duration_ms: Optional[int] = None
+    release_date: Optional[datetime] = None
+    popularity: Optional[float] = None
