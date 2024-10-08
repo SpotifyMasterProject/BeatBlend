ced0a24361f38534640a4acadc75e88c8fdf075f
jemaie <jerome.maier@uzh.ch>
2024-10-08 19:29:34 +0200

    rm ws from active dict

--- server/app/websocket_service.py
+++ server/app/websocket_service.py
@@ -18,11 +18,20 @@ class WebSocketService:
         async with self._manager.subscribe(channel=f"{ws_type}:{session_id}") as subscriber:
             try:
                 async for event in subscriber:
+                    # We technically send a json, but it is in string format. Using "send_json" would
+                    # result in redundant serialization as it was done so before publishing to redis.
                     await websocket.send_text(event.message)
             except WebSocketDisconnect:
-                pass
+                #TODO: remove user from session
+                self._active_connections[session_id].remove(websocket)
+                if not self._active_connections[session_id]:
+                    del self._active_connections[session_id]
+
 
     async def disconnect(self, session_id: str) -> None:
+        if session_id not in self._active_connections:
+            return
+
         for websocket in self._active_connections[session_id]:
             await websocket.close(code=1001, reason='Session ended.')
         del self._active_connections[session_id]
