f692262c2c5bec2e2891fa92b6d49735e22303cc
jemaie <jerome.maier@uzh.ch>
2024-10-08 10:01:06 +0200

    another fe model adj

--- client/src/types/Session.ts
+++ client/src/types/Session.ts
@@ -1,23 +1,28 @@
 import { User } from './User';
 import { Playlist } from '@/types/Playlist';
+import {Recommendation} from "@/types/Recommendation";
 
 export class Session {
     id: string
     name: string
+    hostId: string
     hostName: string
     inviteLink: string
     guests: User[]
     playlist: Playlist[]
     creationDate: Date
+    recommendations: Recommendation[]
 
     constructor(data: Session) {
         this.id = data.id
         this.name = data.name
+        this.hostId = data.hostId
         this.hostName = data.hostName
         this.inviteLink = data.inviteLink
         this.guests = data.guests
         this.playlist = data.playlist
         this.creationDate = data.creationDate
+        this.recommendations = data.recommendations
     }
 }
 
