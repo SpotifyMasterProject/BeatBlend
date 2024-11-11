956238ee19e378f12d7c2f798417484cf4b5156b
DarleneQing <ophlia.cn@gmail.com>
2024-11-11 18:16:00 +0100

    loader icon and fix start session queue

--- client/src/components/PlaylistCreator.vue
+++ client/src/components/PlaylistCreator.vue
@@ -10,6 +10,7 @@ import { Playlist } from "@/types/Playlist";
 const title = ref('');
 const selectedSongs = ref([]);
 const emit = defineEmits(['startSession']);
+const loading = ref(false);
 
 // True, if the session can be started.
 // Currently, if the user selected at least 3 songs and a title.
@@ -18,12 +19,14 @@ const canStartSession = computed(() => {
 });
 
 const startSession = async () => {
+    loading.value = true;
+
     emit('startSession', new Session({
         name: title.value,
         playlist: new Playlist({
             playedSongs: [],
-            currentSong: selectedSongs.value[0],
-            queuedSongs: selectedSongs.value.slice(1),
+            currentSong: null,
+            queuedSongs: selectedSongs.value,
         }),
     }));
 };
@@ -38,6 +41,9 @@ const startSession = async () => {
         <Button class="start-session" :disabled="!canStartSession" @click="startSession">
             Start
         </Button>
+        <div v-if="loading" class="loading-overlay">
+            <div class="spinner"></div>
+        </div>
     </div>
 </template>
 
@@ -103,5 +109,35 @@ input:focus {
 }
 .start-session:disabled {
     color: var(--button-disabled-font-color);
+}
+
+.loading-overlay {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100px;
+    height: 100px;
+    background-color: #363636;
+    border-radius: 12px;
+    z-index: 1000;
+}
+
+.spinner {
+    width: 40px;
+    height: 40px;
+    border: 4px solid rgba(255, 255, 255, 0.3);
+    border-top-color: white;
+    border-radius: 50%;
+    animation: spin 1s linear infinite;
+}
+
+@keyframes spin {
+    to {
+        transform: rotate(360deg);
+    }
 }
 </style>
