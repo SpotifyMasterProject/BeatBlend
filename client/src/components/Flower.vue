0c2edb94ddf53bfcde35ecb29ad37642119eeac6
DarleneQing <ophlia.cn@gmail.com>
2024-11-21 21:06:20 +0100

    Flower highlighting

--- client/src/components/Flower.vue
+++ client/src/components/Flower.vue
@@ -12,6 +12,7 @@ const props = defineProps<{
   circleRadius?: number;
   bloom?: boolean;
   mostSignificantFeature?: SongFeatureCategory
+  isSelected?: boolean;
 }>();
 
 const emit = defineEmits(['onPetalClick', 'significantFeaturePosition', 'hover', 'leave']);
@@ -39,6 +40,13 @@ const onPetalEndPositionComputed = (position, songFeatureCategory) => {
   }
 };
 
+const bloomClass = computed(() => {
+  if (props.bloom && !props.isSelected) return 'bloom-static'; // bloom
+  if (props.isSelected && !props.bloom) return 'bloom-circle';   // Static bloom
+  if (props.bloom && props.isSelected) return 'bloom-static';  // bloom when both are true
+  return ''; // No bloom effect
+});
+
 onMounted(() => {
   if (props.mostSignificantFeature === null) {
     emit('significantFeaturePosition', {x: maxPetalLength.value, y: maxPetalLength.value});
@@ -64,9 +72,16 @@ onMounted(() => {
     <!-- Define the glowing effect -->
     <defs>
       <filter id="circle-bloom">
-        <feGaussianBlur in="SourceGraphic" stdDeviation="7" result="blur" />
-        <feColorMatrix type="matrix" values="3 0 0 0 0  0 3 0 0 0  0 0 3 0 0  0 0 0 1 0" result="brightBlur" />
+        <feGaussianBlur in="SourceGraphic" stdDeviation="3" result="blur1" />
+        <feGaussianBlur in="SourceGraphic" stdDeviation="7" result="blur2" />
+        <feMerge result="combinedBlur">
+          <feMergeNode in="blur1" />
+          <feMergeNode in="blur2" />
+        </feMerge>
+        <feColorMatrix type="matrix" values="8 0 0 0 0  0 8 0 0 0  0 0 8 0 0  0 0 0 1 0" result="brightBlur" />
+        <feComposite in="brightBlur" in2="SourceGraphic" operator="arithmetic" k1="1" k2="1" k3="0" k4="0" result="compositeGlow" />
         <feMerge>
+          <feMergeNode in="compositeGlow" />
           <feMergeNode in="brightBlur" />
           <feMergeNode in="SourceGraphic" />
         </feMerge>
@@ -80,8 +95,7 @@ onMounted(() => {
         fill="none"
         stroke="#CCCCCC"
         stroke-width="1"
-        :filter="props.bloom ? 'url(#circle-bloom)' : null"
-        :class="{ 'bloom-circle': props.bloom }"
+        :class="bloomClass"
     />
     <Petal
         class="petal"
@@ -121,6 +135,10 @@ svg {
 
 .bloom-circle {
   animation: bloom 1s infinite ease-in-out;
+  filter: url(#circle-bloom);
+}
+.bloom-static {
+  filter: url(#circle-bloom);
 }
 .flower {
   cursor: pointer;
