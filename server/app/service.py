ea0bd2c8c9819a6c475e16c7594c33e72b66d0f6
maettuu <mangori41@bluewin.ch>
2024-12-04 12:36:04 +0100

    adjust default values

--- server/app/service.py
+++ server/app/service.py
@@ -333,13 +333,13 @@ class Service:
         return Artifact(
             songs_played=total_songs,
             songs_added_manually=total_manually_added_songs,
-            most_songs_added_by=[user.username for user in most_songs_added_by] if most_songs_added_by else None,
-            most_votes_by=[user.username for user in most_votes_by] if most_votes_by else None,
+            most_songs_added_by=[user.username for user in most_songs_added_by] if most_songs_added_by else [],
+            most_votes_by=[user.username for user in most_votes_by] if most_votes_by else [],
             most_significant_feature_overall=most_significant_feature_overall,
             first_recommendation_vote_percentage=round(first_recommendation_vote_percentage, 1),
             average_features=average_features,
-            genre_start=played_songs[0].genre if played_songs else None,
-            genre_end=played_songs[-1].genre if played_songs else None
+            genre_start=played_songs[0].genre if played_songs else [],
+            genre_end=played_songs[-1].genre if played_songs else []
         )
 
     @with_session_lock
